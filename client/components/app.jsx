b9a204689416fbca90a88196bd74b86090ddf1c7
Nathan Reitan <nathanreitan@gmail.com>
2020-04-18 11:15:08 -0700

    get request successful

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+    this.getGrades = this.getGrades.bind(this);
+  }
+
+  componentDidMount() {
+    this.getGrades();
+  }
+
+  getGrades() {
+    fetch('/api/grades')
+      .then(resp => { return resp.json(); })
+      .then(data => {
+        return this.setState(state => ({
+          grades: data
+        }));
+      });
+  }
+
   render() {
-    return null;
+    return (
+      <Header/>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return <h1>Student Grade Table</h1>;
   }
 }
 
