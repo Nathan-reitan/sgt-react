a2bd57b0ca6508835cc45ab34a41496559e484bf
Nathan Reitan <nathanreitan@gmail.com>
2020-04-18 12:08:36 -0700

    user-can-view-grades ready for review

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -20,12 +20,20 @@ class App extends React.Component {
         return this.setState(state => ({
           grades: data
         }));
-      });
+      })
+      .catch(error => console.error('Error:', error));
   }
 
   render() {
     return (
-      <Header/>
+      <div className="container">
+        <div className="row">
+          <div className="col pt-5">
+            <Header/>
+            <GradeTable grades={this.state.grades}/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
@@ -34,6 +42,45 @@ class Header extends React.Component {
   render() {
     return <h1>Student Grade Table</h1>;
   }
+}
+
+function Grade(props) {
+  const grade = props.grade;
+  const idAttr = `${grade.id}`;
+  return (
+    <tr id={idAttr}>
+      <th scope="row">{idAttr}</th>
+      <td>{grade.name}</td>
+      <td>{grade.course}</td>
+      <td>{grade.grade}</td>
+    </tr>
+  );
+}
+
+function GradeTable(props) {
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope='col'>Student ID</th>
+          <th scope='col'>Student Name</th>
+          <th scope='col'>Course</th>
+          <th scope='col'>Grade</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.grades.map(grade => {
+            return (
+              <Grade
+                key={grade.id}
+                grade={grade}/>
+            );
+          })
+        }
+      </tbody>
+    </table>
+  );
 }
 
 export default App;
