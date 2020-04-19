47441200907debe3b609bc4498e4291b12f8db41
Nathan Reitan <nathanreitan@gmail.com>
2020-04-19 12:59:00 -0700

    average fixed to round to integer

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -37,7 +37,7 @@ class App extends React.Component {
     if (!average) {
       return 'Calculating';
     }
-    return average / grades.length;
+    return parseInt(average / grades.length);
   }
 
   addGrades(newGrade) {
