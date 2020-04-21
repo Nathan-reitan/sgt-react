f4dc2f177fd0c1e78eb535acb729769f0ccddd92
Nathan Reitan <nathanreitan@gmail.com>
2020-04-21 16:16:44 -0700

    styling changes, comment out incomplete code

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Header from './header';
 import GradeTable from './gradeTable';
 import GradeForm from './gradeForm';
+import ReviseForm from './reviseForm';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grades: []
+      grades: [],
+      currentlyEditing: false
     };
     this.getGrades = this.getGrades.bind(this);
     this.addGrades = this.addGrades.bind(this);
     this.deleteGrades = this.deleteGrades.bind(this);
+    this.reviseGrade = this.reviseGrade.bind(this);
+    this.toggleEditing = this.toggleEditing.bind(this);
+    this.modalRef = React.createRef();
   }
 
   componentDidMount() {
@@ -79,14 +84,51 @@ class App extends React.Component {
       .catch(error => console.error('Error:', error));
   }
 
+  // reviseGrade(gradeId, updGrade) {
+  //   const prevGrades = this.state.grades.slice();
+  //   fetch(`/api/grades/${gradeId}`, {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(updGrade)
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => console.log(data));
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     for (let i = 0; i < prevGrades.length; i++) {
+  //       if (prevGrades[i].id === gradeId) {
+  //         prevGrades[i] = data;
+  //         return prevGrades;
+  //       }
+  //     }
+  //   })
+  //   .then(newArray => { return this.setState(state => ({ grades: newArray })); })
+  //   .catch(error => console.error('Error:', error));
+  // }
+
+  toggleEditing() {
+    return this.setState(state => ({ currentlyEditing: !state.currentlyEditing }));
+  }
+
   render() {
+    const modalClass = this.state.currentlyEditing
+      ? ''
+      : 'hidden';
     return (
       <div className="container containerBackground shadow-sm">
+        <div className="modalOverlay d-flex flex-column">
+          <div ref={this.modalRef}className={`modalContent form ${modalClass}`}>
+            <h5>Revise grade here.</h5>
+            <ReviseForm editing={this.state.currentlyEditing}/>
+          </div>
+        </div>
         <div className="col pt-5">
           <Header average={this.getAverageGrade()}/>
           <div className="row">
             <div className="col-7">
-              <GradeTable grades={this.state.grades} remove={this.deleteGrades} />
+              <GradeTable grades={this.state.grades} remove={this.deleteGrades} editing={this.toggleEditing} />
             </div>
             <div className="col-5 form">
               <GradeForm onSubmit={this.addGrades} />
---
+++ client/components/reviseForm.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+export default class ReviseForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.editing = this.props.currentlyEditing;
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newGrade = {
+      name: this.state.name,
+      course: this.state.course,
+      grade: this.state.grade
+    };
+    this.props.onSubmit(newGrade);
+    this.setState({
+      name: '',
+      course: '',
+      grade: ''
+    });
+  }
+
+  handleReset(event) {
+    event.preventDefault();
+    this.setState({
+      name: '',
+      course: '',
+      grade: ''
+    });
+  }
+
+  render() {
+    const name = this.state.name;
+    const course = this.state.course;
+    const grade = this.state.grade;
+    return (
+      <form className="input-group mb-4" onSubmit={this.handleSubmit}>
+        <label>
+          <div className="fas fa-user"></div>
+          <input
+            className="mx-4"
+            type="text"
+            id="name"
+            value={name}
+            placeholder="Name"
+            onChange={this.handleChange} />
+        </label>
+        <label>
+          <div className="far fa-list-alt"></div>
+          <input
+            className="mx-4"
+            type="text"
+            id="course"
+            value={course}
+            placeholder="Course"
+            onChange={this.handleChange} />
+        </label>
+        <label>
+          <div className="fas fa-graduation-cap"></div>
+          <input
+            className="mx-4"
+            type="text"
+            id="grade"
+            value={grade}
+            placeholder="Grade"
+            onChange={this.handleChange} />
+        </label>
+        <button type="submit" className="btn btn-primary" onClick={this.populateModal}>Submit Revision</button>
+        <button type="reset" className="mx-2 btn btn-secondary" onClick={this.handleReset}>Cancel</button>
+      </form>
+    );
+  }
+}
